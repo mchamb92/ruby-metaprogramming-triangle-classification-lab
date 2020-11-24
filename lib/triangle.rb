7fb2f9d7aef6b7ea45144f0635eb435d2d70d43a
Mustafa <mchambqa@gmail.com>
2020-11-24 01:08:47 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sides = [side1,side2,side3]
+    if sides.any?{|side| side <= 0} || sides.max >= (side1+side2+side3) - sides.max
+        raise TriangleError
+    elsif side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    #def message
+    #  "This is not a valid triangle"
+    #end
+  end
 end
